c66883a98024914897b221b22c87cf9a0ab15c43
Josephine19001 <gyameraj@gmail.com>
2021-01-29 02:20:35 +0200

    removed unused useeffect

--- client/src/pages/Home/Checkout.tsx
+++ client/src/pages/Home/Checkout.tsx
@@ -1,4 +1,4 @@
-import React, { useEffect } from "react";
+import React from "react";
 
 import UserLayout from "../../components/layout/UserLayout";
 import Checkout from "../../components/Checkout/Checkout";
