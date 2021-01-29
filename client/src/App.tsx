a1b49533aca12022a81aca6406f7f1b33544ddad
Josephine19001 <gyameraj@gmail.com>
2021-01-29 02:13:37 +0200

    removed FC react import

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,4 +1,4 @@
-import React, { FC, useEffect } from "react";
+import React, { useEffect } from "react";
 import { BrowserRouter as Router } from "react-router-dom";
 import { useDispatch } from "react-redux";
 
